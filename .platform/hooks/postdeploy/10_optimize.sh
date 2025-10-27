b3f666d8565abdd9986067f2330146e585c7247f
sarata0101 <ssaattaa0011@gmail.com>
2025-10-27 20:39:51 +0300

    Use /var/www/html/public + add postdeploy hook (LF, executable)

--- .platform/hooks/postdeploy/10_optimize.sh
+++ .platform/hooks/postdeploy/10_optimize.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 cd /var/app/current
-
 php artisan storage:link || true
-php artisan migrate --force
+php artisan migrate --force || true
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
